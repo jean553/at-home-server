a129f6b1238aeedcf10516a912671f18ccea6ba0
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2019-06-29 13:22:22 +0200

    attempt to send a single text message with Rusoto and SNS

--- at-home-server/src/main.rs
+++ at-home-server/src/main.rs
@@ -8,9 +8,26 @@ extern crate serde_json;
 extern crate rocket_contrib;
 extern crate uuid;
 extern crate redis;
+extern crate futures;
+extern crate rusoto_core;
+extern crate rusoto_credential;
+extern crate rusoto_sns;
+
+extern crate env_logger;
 
 mod ride;
 mod rides;
+
+use rusoto_core::{
+    Region,
+    HttpClient,
+};
+use rusoto_sns::{
+    Sns,
+    SnsClient,
+    PublishInput,
+};
+use rusoto_credential::EnvironmentProvider;
 
 use std::env;
 
@@ -24,6 +41,27 @@ fn main() {
 
     let redis_url: &str = &env::var("REDIS_URL").expect("Missing REDIS_URL");
     let redis_client = redis::Client::open(redis_url).unwrap();
+
+    let credentials = EnvironmentProvider::default();
+
+    let sns_client = SnsClient::new_with(
+        HttpClient::new().unwrap(),
+        credentials,
+        Region::EuWest1
+    );
+
+    /* FIXME: sending a text message is handled here for now for tests purposes,
+       it should be sent when an user is arrived at his final destination */
+    let message = PublishInput {
+        message: "Your friend is at home!".to_string(),
+        phone_number: Some("".to_string()),
+        message_attributes: None,
+        message_structure: None,
+        subject: Some("AtHome".to_string()),
+        topic_arn: None,
+        target_arn: None,
+    };
+    sns_client.publish(message).sync().unwrap();
 
     rocket::ignite()
         .manage(redis_client)
